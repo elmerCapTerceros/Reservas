f7339842c9e5263cb5222259566be6085b55c7e0
andres7o7 <andresrevollohuanca@gmail.com>
2023-10-25 00:40:12 -0400

    admin busqueda

--- server.js
+++ server.js
@@ -1,132 +1,44 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const app = express();
+const port = 5500;// Puertos que deseas usar
+const sql = require('mssql');
+const cors = require('cors');
 
 
-// Conecta a MongoDB
-mongoose.connect('mongodb://localhost/ReservaAulas', { useNewUrlParser: true });
+app.use(cors());
 
-// Configura sesiones y passport para autenticación
-app.use(session({ secret: 'mi-secreto', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+const config = {
+    server: 'localhost',
+    user: 'andres',
+    password: 'estopa',
+    database: 'ReservaProyecto',
+    options: {
+        "encrypt": true,
+        enableArithAbort: true, // o false, según tus necesidades
+      },
+  };
 
+// Configuración de Express para servir archivos estáticos (tu página HTML)
+app.use(express.static('public')); // Asumiendo que los archivos HTML están en una carpeta llamada 'public'
 
-const reserva = mongoose.model('reserva', {
-    nombreCompleto: String,
-    fecha: Date,
-    grupo: Number,
-    horario: String,
-    motivo: String,
-    materia: String,
-});
-
-const aula = mongoose.model('aula', {
-    nameAula: String,
-    capacidad: Number,
-    descripcion: String,
-});
-
-// Configura la estrategia de autenticación local con Passport
-passport.use(new LocalStrategy((username, password, done) => {
-    // Aquí debes verificar si el usuario y la contraseña son correctos en tu base de datos
-    // Si los datos son correctos, puedes llamar a done(null, user)
-    // Si no, puedes llamar a done(null, false) o done(err) si hay un error
-}));
-
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
-
-// Middlewares para procesar datos JSON y formularios
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Rutas
-app.get('/reservas', async (req, res) => {
+// Ruta para obtener los datos de la tabla 'Aula'
+app.get('/obtener-datos-aula', async (req, res) => {
     try {
-        const reservas = await Reserva.find();
-        res.json(reservas);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al buscar reservas' });
+      await sql.connect(config);
+  
+      const result = await sql.query('SELECT codAula, Capacidad, Descripcion FROM Aula');
+  
+      const datosAula = result.recordset; // Los datos se obtienen del resultado de la consulta
+  
+      res.json(datosAula);
+    } catch (err) {
+      console.error('Error al obtener datos de la base de datos:', err);
+      res.status(500).send('Error interno del servidor');
+    } finally {
+      sql.close();
     }
-});
-
-app.post('/reservas', async (req, res) => {
-    const reservaData = req.body;
-    const reserva = new reserva(reservaData);
-
-    try {
-        const nuevaReserva = await reserva.save();
-        res.json(nuevaReserva);
-    } catch (error) {
-        res.status(400).json({ error: 'Error al crear la reserva' });
-    }
-});
-
-app.put('/reservas/:id', async (req, res) => {
-    const reservaData = req.body;
-    const id = req.params.id;
-
-    try {
-        const reservaActualizada = await Reserva.findByIdAndUpdate(id, reservaData, { new: true });
-        res.json(reservaActualizada);
-    } catch (error) {
-        res.status(400).json({ error: 'Error al actualizar la reserva' });
-    }
-});
-
-app.delete('/reservas/:id', async (req, res) => {
-    const id = req.params.id;
-
-    try {
-        await Reserva.findByIdAndDelete(id);
-        res.json({ message: 'Reserva eliminada con éxito' });
-    } catch (error) {
-        res.status(400).json({ error: 'Error al eliminar la reserva' });
-    }
-});
-
-
-app.get('/ver-reservas', async (req, res) => {
-    try {
-        const reservas = await reserva.find(); // Recupera todos los documentos de la colección "reservas"
-        res.render('ver-reservas', { reservas }); // Renderiza una vista o devuelve los datos como JSON
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener reservas' });
-    }
-});
-
-app.post('/login',
-    passport.authenticate('local', {
-        successRedirect: '/dashboard',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
-);
-
-app.get('/dashboard', (req, res) => {
-    // Ruta protegida, solo accesible para usuarios autenticados
-});
-
-app.get('/aulas', async (req, res) => {
-    try {
-        const aulas = await aula.find();
-        res.json(aulas);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al buscar aulas' });
-    }
-});
-
-// Inicia el servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor en ejecución en el puerto ${PORT}`);
-});
+  });
+  
+  app.listen(port, () => {
+    console.log(`Servidor en ejecución en http://localhost:${port}`);
+  });
