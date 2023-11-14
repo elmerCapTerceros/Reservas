b12e621df56bd084e3cf7d00c89b7840494809da
Elmer Jimenez <elmerjimenez6556@gmail.com>
2023-11-13 22:49:43 -0400

    add.js

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
+const express = require('express');
+const app = express();
+const port = 5500;
 const sql = require('mssql');
+const cors = require('cors');
 
+const multer = require('multer'); // Para manejar archivos en Express
+const csv = require('csv-parser'); // Para analizar archivos CSV
+const fs = require('fs'); 
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(cors());
+app.use(express.json());
 const config = {
     server: 'localhost',
-    user: 'andres',
+    user: 'capTerceros',
     password: 'estopa',
     database: 'ReservaProyecto',
     options: {
         "encrypt": true,
-        enableArithAbort: true, // o false, según tus necesidades
-      },
-  };
+        enableArithAbort: true,
+    },
+};
 
-  async function insertAula() {
+app.use(express.static('public'));
+
+app.get('/obtener-datos-aula', async (req, res) => {
     try {
         await sql.connect(config);
-
-        const request = new sql.Request();
-        request.input('codAula', sql.NVarChar, '690A');
-        request.input('Capacidad', sql.Int, 100);
-        request.input('Descripcion', sql.NVarChar, 'Aula grande con proyector');
-
-        const query = 'INSERT INTO Aula (codAula, Capacidad, Descripcion) VALUES (@codAula, @Capacidad, @Descripcion)';
-        const result = await request.query(query);
-
-        console.log('Registro insertado correctamente.');
-
+        const result = await sql.query('SELECT codAula, Capacidad, Descripcion FROM Aula');
+        const datosAula = result.recordset;
+        res.json(datosAula);
     } catch (err) {
-        console.error('Error al insertar registro:', err);
+        console.error('Error al obtener datos de la base de datos:', err);
+        res.status(500).send('Error interno del servidor');
     } finally {
         sql.close();
     }
-}
+});
 
-insertAula();
+app.post('/agregar-datos-aula', async (req, res) => {
+  try {
+      const { codAula, Capacidad, Descripcion } = req.body;
+
+      if (!codAula || !Capacidad || !Descripcion) {
+          return res.status(400).json({ error: 'Faltan datos obligatorios' });
+      }
+
+      await sql.connect(config);
+
+      const query = `INSERT INTO Aula (codAula, Capacidad, Descripcion) VALUES ('${codAula}', ${Capacidad}, '${Descripcion}')`;
+      const result = await sql.query(query);
+
+      if (result.rowsAffected[0] === 1) {
+          res.status(201).json({ message: 'Datos de aula agregados exitosamente' });
+      } else {
+          res.status(500).json({ error: 'No se pudieron agregar los datos de aula' });
+      }
+  } catch (err) {
+      console.error('Error al agregar datos de aula a la base de datos:', err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+  } finally {
+      sql.close();
+  }
+});
+
+
+app.post('/procesar-archivo-csv', upload.single('archivo'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+      return res.status(400).json({ error: 'No se ha proporcionado un archivo CSV' });
+  }
+
+  const data = file.buffer.toString('utf8');
+
+  
+  const results = [];
+  fs.createReadStream(data)
+      .pipe(csv())
+      .on('data', (row) => {
+          results.push(row);
+      })
+      .on('end', () => {
+        
+
+          res.status(200).json({ message: 'Datos agregados desde el archivo CSV' });
+      });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
---
+++ public/add.js
@@ -0,0 +1,25 @@
+//fetch('http://localhost:5500/obtener-datos-aula').then(response => response.json()).then(data => console.log(data));
+
+
+const boton = document.getElementById("botons");
+const agregar = ()=>{
+    let codAula=document.getElementById('codAula').value;
+    let Capacidad=document.getElementById('Capacidad').value;
+    let Descripcion=document.getElementById('Descripcion').value;
+
+    const nuevo = {
+        codAula,
+        Capacidad,
+        Descripcion
+    }
+    fetch('http://localhost:5500/agregar-datos-aula', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(nuevo)
+    })
+}
+
+boton.addEventListener('click',agregar);
+
