832a5d495a39a8562cea8ee8430621a652ae6922
andres7o7 <andresrevollohuanca@gmail.com>
2023-11-15 01:52:43 -0400

    busquedaAdmin values de los campos del modal

--- public/busquedaAdmi.js
+++ public/busquedaAdmi.js
@@ -1,3 +1,6 @@
+const botonmodal = document.getElementById("modificar");
+
+
 fetch('http://localhost:5500/obtener-datos-aula')
       .then(response => response.json())
       .then(data => {
@@ -14,7 +17,7 @@ fetch('http://localhost:5500/obtener-datos-aula')
           codAulaCell.textContent = item.codAula;
           capacidadCell.textContent = item.Capacidad + " alumnos";
           descripcionCell.textContent = item.Descripcion;
-          modificar.innerHTML = `<button onclick="document.getElementById('id01').style.display='block'" class="modificar-btn "><i class="fa fa-edit"></i></button>`;
+          modificar.innerHTML = `<button onclick="abrirModal(this)" class="modificar-btn"><i class="fa fa-edit"></i></button>`;
           estado.textContent = 'Habilitado';
           console.log('biem');
         });
@@ -23,20 +26,62 @@ fetch('http://localhost:5500/obtener-datos-aula')
         console.error("Error al obtener datos:", error);
       });
 
-      const modificarBtns = document.querySelectorAll('.modificar-btn');
-
-  modificarBtns.forEach(btn => {
-   btn.addEventListener('click', () => {
-     modificarAula(btn);
       
-   });
-   
-  });
+  
 
   function modificarAula(btn) {
     // código para modificar el aula
    
   }
 
+  const abrirModal = (button) => {
+    const row = button.closest('tr');
+    const titulo = row.cells[0].textContent;
+    const capacidad = row.cells[1].textContent.split(' ')[0]; // Obtener solo el número de capacidad
+    const descripcion = row.cells[2].textContent;
   
+    // Asignar los valores al formulario en el modal
+    document.getElementById('capacidad').value = capacidad;
+    document.getElementById('descripcion').value = descripcion;
+    document.getElementById('tituloModal').textContent = 'Actualizar aula : '+titulo;
+  
+    // Mostrar el modal
+    document.getElementById('id01').style.display = 'block';
+  };
+  
+  const modificar = document.querySelectorAll('.modificar-btn');
+  
+  modificar.forEach(btn => {
+    btn.addEventListener('click', () => {
+      abrirModal(btn);
+    });
+  });
 
+
+
+  const contenidoform = () => {
+    const Capacidad = document.getElementById('capacidad').value;
+    const Descripcion = document.getElementById('descripcion').value;
+    const codAula = document.getElementById('tituloModal').textContent;
+  
+    // Enviar la solicitud PUT al servidor
+    fetch(`http://localhost:5500/aulas/${codAula}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ codAula,Capacidad, Descripcion }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Actualizar la interfaz de usuario según sea necesario
+        console.log('Aula actualizada:', data);
+      })
+      .catch(error => {
+        console.error('Error al actualizar aula:', error);
+      });
+  };
+  
+  const boton = document.getElementById("modificar");
+  boton.addEventListener('click', contenidoform);
+  
