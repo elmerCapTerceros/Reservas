9b5b4b77fae4c6d80d5f1f910c92b169451c6b0a
Elmer Jimenez <elmerjimenez6556@gmail.com>
2023-12-05 11:15:08 -0400

    Reservas

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 const config = {
     server: 'localhost',
-    user: 'andres',
+    user: 'capTerceros',
     port: 1433,
     password: 'estopa',
     database: 'ReservaProyecto',
@@ -114,7 +114,23 @@ app.post('/agregar-datos-aula', async (req, res) => {
   }
 });
 
+//quiero solo los cursos, para la reserva.
+app.get('/obtenerAulas', async (req, res) => {
+  try {
+    await sql.connect(config);
 
+    const result = await sql.query('SELECT codAula, Capacidad FROM Aula');
+
+    const datosAula = result.recordset; // Los datos se obtienen del resultado de la consulta
+
+    res.json(datosAula);
+  } catch (err) {
+    console.error('Error al obtener datos de la base de datos:', err);
+    res.status(500).send('Error interno del servidor');
+  } finally {
+    sql.close();
+  }
+});
 
 app.post('/codDocente', async (req, res) => {
   try {
@@ -161,6 +177,37 @@ app.get('/prueba',(req,res,next)=>{
   console.log(decoded);
   res.json('tienes autorizacion');
 })
+//reservas
+
+app.post('/nuevaReserva', async (req, res) => {
+  try {
+      const { nombreDocente, motivo, fechaReserva, grupo, horario, materia, codAula } = req.body;
+
+      if (!nombreDocente || !motivo || !fechaReserva || !grupo || !horario || !materia || !codAula) {
+          return res.status(400).json({ error: 'Faltan datos obligatorios' });
+      }
+
+    // Realiza la transformación condicional según el valor de 'horario'
+
+     await sql.connect(config);
+      const query = `INSERT INTO Reservas (nombreDocente, motivo, fechaReserva, grupo, horario, materia, codAula) VALUES ('${nombreDocente}', '${motivo}', '${fechaReserva}',${grupo},'${horario}','${materia}','${codAula}')`;
+      const result = await sql.query(query);
+
+      if (result.rowsAffected[0] === 1) {
+          res.status(201).json({ message: 'Reserva Agregada' });
+      } else {
+          res.status(500).json({ error: 'No se pudieron agregar los datos de la reserva' });
+      }
+  } catch (err) {
+      console.error('Error al agregar datos de la reserva a la base de datos:', err);
+      res.status(500).json({ error: 'Error interno del servidor' });
+  } finally {
+      sql.close();
+  }
+});
+
+
+
 
   app.listen(port, () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
